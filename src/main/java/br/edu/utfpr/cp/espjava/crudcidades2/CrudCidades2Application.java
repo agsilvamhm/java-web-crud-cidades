954d367883d699c5b7570fa347fa4b024e813df2
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2024-05-04 07:49:26 -0300

    Primeiro commit do projeto, funcionando com a página statica

---
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades2/CrudCidades2Application.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.cp.espjava.crudcidades2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCidades2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudCidades2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/utfpr/cp/espjava/crudcidades2/CrudCidades2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.cp.espjava.crudcidades2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCidades2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
