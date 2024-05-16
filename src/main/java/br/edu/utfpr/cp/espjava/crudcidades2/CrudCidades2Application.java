252b6677ef89f9a1fab2f1950c70226ca8641fc7
agsilvamhm <adalberto.goncalves.silva@gmail.com>
2024-05-15 21:27:43 -0300

    Validações implementadas

--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades2/CrudCidades2Application.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades2/CrudCidades2Application.java
@@ -2,12 +2,32 @@ package br.edu.utfpr.cp.espjava.crudcidades2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class CrudCidades2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudCidades2Application.class, args);
+	}
+
+	@Bean
+	public MessageSource messageSource(){
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+		messageSource.setBasename("classpath:messages");
+		messageSource.setDefaultEncoding("UTF-8");
+		return messageSource;
+	}
+
+	@Bean
+	public Validator getValidator(){
+		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
+		bean.setValidationMessageSource(messageSource());
+		return bean;
 	}
 
 }
--- src/main/java/br/edu/utfpr/cp/espjava/crudcidades2/visao/CidadeController.java
+++ src/main/java/br/edu/utfpr/cp/espjava/crudcidades2/visao/CidadeController.java
@@ -1,7 +1,9 @@
 package br.edu.utfpr.cp.espjava.crudcidades2.visao;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,19 +19,28 @@ public class CidadeController {
     }
     @GetMapping("/")
     public String listar(Model memoria){
-     //   var cidades = Set.of(
-      //  new Cidade("Cornélio Procópio", "PR"),
-     //   new Cidade("Assis", "SP"),
-     //   new Cidade("Itajaí", "SC"),
-     //   new Cidade("Campina Grande", "PB")
-      //  );
         memoria.addAttribute("listaCidades", cidades);
         return "/crud";
     }
 
     @PostMapping("/criar")
-    public String criar(Cidade cidade){
-        cidades.add(cidade);
+    public String criar(@Valid Cidade cidade, BindingResult validacao, Model memoria){
+        if (validacao.hasErrors()){
+            validacao
+                    .getFieldErrors()
+                    .forEach(error ->
+                            memoria.addAttribute(
+                                            error.getField(),
+                                            error.getDefaultMessage())
+                            );
+            memoria.addAttribute("nomeInformado", cidade.getNome());
+            memoria.addAttribute("estadoInformado", cidade.getEstado());
+            memoria.addAttribute("listaCidades", cidades);
+            return ("/crud");
+        }else {
+            cidades.add(cidade);
+        }
+
         return "redirect:/";
     }
 
@@ -71,14 +82,16 @@ public class CidadeController {
     public String alterar(
             @RequestParam String nomeAtual,
             @RequestParam String estadoAtual,
-            Cidade cidade
-    ){
+            Cidade cidade,
+            BindingResult validacao,
+            Model memoria) {
+
         cidades.removeIf(cidadeAtual ->
                 cidadeAtual.getNome().equals(nomeAtual) &&
-                cidadeAtual.getEstado().equals(estadoAtual)
-        );
+                        cidadeAtual.getEstado().equals(estadoAtual));
 
-        criar(cidade);
+        criar(cidade, validacao, memoria);
+
         return "redirect:/";
     }
 }
